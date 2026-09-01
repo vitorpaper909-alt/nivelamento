7f3fd824bf12ce9032865a6dfb3b1399482534f5
vitorpaper909-alt <vitorpaper909@gmail.com>
2026-09-01 09:03:06 -0300

    ok

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const likebtn = document.querySelector('.like-btn');
+    const postMedia = document.querySelector('.post-media');
+if (!likebtn) return;
+
+const likesCountSpam = likebtn.querySelector('.likes-count');
+const likesCountSpan = likebtn.querySelector('.bookmark-btn');
+
+
+
+
+
+
+})
